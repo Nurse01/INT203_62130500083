fa3a5a9615d8c15d2fb15940e1966f81db870d83
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-01-31 23:53:33 +0700

    Finish

--- main.js
+++ main.js
@@ -1,53 +1,72 @@
 let indexOfImg;
-const mainPic = document.getElementById('mainpic');
-const pic1 = document.getElementById('pic1');
-const pic2 = document.getElementById('pic2');
-const pic3 = document.getElementById('pic3');
-const pic4 = document.getElementById('pic4');
-const pic5 = document.getElementById('pic5');
-const pic6 = document.getElementById('pic6');
-const srcElem = document.createAttribute('src');
-const picBorderElem = document.getElementsByClassName('pic-border');
-
+const mainPic = document.getElementById("mainpic");
+const pic1 = document.getElementById("pic1");
+const pic2 = document.getElementById("pic2");
+const pic3 = document.getElementById("pic3");
+const pic4 = document.getElementById("pic4");
+const pic5 = document.getElementById("pic5");
+const pic6 = document.getElementById("pic6");
+const srcElem = document.createAttribute("src");
 
 
 function clickpic(indexOfImg) {
-
-    document.documentElement.style.setProperty('--border-color',"var(--pic-border)");
-    if(indexOfImg == 1){ 
-        mainPic.removeAttribute('src');
+    pic1.style.setProperty('--border-color', "var(--non-pic-border)");
+    pic2.style.setProperty('--border-color', "var(--non-pic-border)");
+    pic3.style.setProperty('--border-color', "var(--non-pic-border)");
+    pic4.style.setProperty('--border-color', "var(--non-pic-border)");
+    pic5.style.setProperty('--border-color', "var(--non-pic-border)");
+    pic6.style.setProperty('--border-color', "var(--non-pic-border)");
+    let focusing;
+    switch (indexOfImg) {
+        case 1:
+            focusing = pic1;
+            break;
+        case 2:
+            focusing = pic2;
+            break;
+        case 3:
+            focusing = pic3;
+            break;
+        case 4:
+            focusing = pic4;
+            break;
+        case 5:
+            focusing = pic5;
+            break;
+        case 6:
+            focusing = pic6;
+            break;
+    }
+    focusing.style.setProperty("--border-color", "var(--pic-border)");
+    if (indexOfImg == 1) {
+        mainPic.removeAttribute("src");
         srcElem.value = "images/nk-1.jpg";
         mainPic.setAttributeNode(srcElem);
-        
-
-    }else{
-        mainPic.removeAttribute('src');
+    } else {
+        mainPic.removeAttribute("src");
         srcElem.value = `images/nk-${indexOfImg}.png`;
         mainPic.setAttributeNode(srcElem);
     }
-
 }
 
 function run() {
     pic1.addEventListener("click", () => {
-        clickpic(1)
+        clickpic(1);
     });
     pic2.addEventListener("click", () => {
-        clickpic(2)
+        clickpic(2);
     });
     pic3.addEventListener("click", () => {
-        clickpic(3)
+        clickpic(3);
     });
     pic4.addEventListener("click", () => {
-        clickpic(4)
+        clickpic(4);
     });
     pic5.addEventListener("click", () => {
-        clickpic(5)
+        clickpic(5);
     });
     pic6.addEventListener("click", () => {
-        clickpic(6)
+        clickpic(6);
     });
-
-
 }
 run();
